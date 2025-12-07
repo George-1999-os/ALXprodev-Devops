0f1ff29a116f6929770b502394181de056cd262f
George-1999-os <georgeibuchi44@gmail.com>
2025-12-07 18:13:01 +0100

    Task 1: Add checker-compliant parse_pokemon.sh

--- Advanced_shell/data_extraction_automation-0x01/parse_pokemon.sh
+++ Advanced_shell/data_extraction_automation-0x01/parse_pokemon.sh
@@ -9,11 +9,11 @@ if [ -z "$INPUT" ] || [ ! -f "$INPUT" ]; then
 fi
 
 name=$(jq -r '.name' "$INPUT" | awk '{print toupper(substr($0,1,1)) tolower(substr($0,2))}')
+type=$(jq -r '.types[0].type.name' "$INPUT" | awk '{print toupper(substr($0,1,1)) tolower(substr($0,2))}')
 height=$(jq -r '.height' "$INPUT")
 weight=$(jq -r '.weight' "$INPUT")
-type=$(jq -r '.types[0].type.name' "$INPUT" | awk '{print toupper(substr($0,1,1)) tolower(substr($0,2))}')
 
 formatted_height=$(awk "BEGIN {printf \"%.1f\", $height/10}")
-formatted_weight=$(awk "BEGIN {printf \"%.1f\", $weight/10}")
+formatted_weight=$(awk "BEGIN {w=$weight/10; printf \"%g\", w}")
 
 echo "$name is of type $type, weighs ${formatted_weight}kg, and is ${formatted_height}m tall."
