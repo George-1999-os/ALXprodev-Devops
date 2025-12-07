5f15b8d91b71c77367098f98163f2d2df9adbaec
George-1999-os <georgeibuchi44@gmail.com>
2025-12-07 17:27:30 +0100

    Task 1: Add data_extraction_automation-0x01 and parse_pokemon.sh

--- Advanced_shell/data_extraction_automation-0x01/parse_pokemon.sh
+++ Advanced_shell/data_extraction_automation-0x01/parse_pokemon.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
-
 # Usage: ./parse_pokemon.sh <pokemon.json>
 INPUT="$1"
 if [ -z "$INPUT" ] || [ ! -f "$INPUT" ]; then
     echo "Usage: $0 <pokemon.json>"
     exit 1
 fi
-
 name=$(jq -r '.name' "$INPUT" | sed 's/.*/\u&/')
 height=$(jq -r '.height' "$INPUT")
 weight=$(jq -r '.weight' "$INPUT")
 type=$(jq -r '.types[0].type.name' "$INPUT" | sed 's/.*/\u&/')
-
 formatted_height=$(awk "BEGIN {printf \"%.1f\", $height/10}")
 formatted_weight=$(awk "BEGIN {printf \"%.1f\", $weight/10}")
-
 echo "$name is of type $type, weighs ${formatted_weight}kg, and is ${formatted_height}m tall."
